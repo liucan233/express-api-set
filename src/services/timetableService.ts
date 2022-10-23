64da040126034335e787ee9b85d3c10cbfdd9903
liucan <liucan2333@qq.com>
2022-10-23 16:30:52 +0800

    feat: 完全统一实验课和教务处课接口返回结构

--- src/services/timetableService.ts
+++ src/services/timetableService.ts
@@ -3,6 +3,13 @@ import logger from "jet-logger";
 import cookieUtil from "cookie";
 import { load } from "cheerio";
 import { map } from "cheerio/lib/api/traversing";
+import {
+  covertJwCourse,
+  covertLabCourse,
+  IJWCourse,
+  ILabCourse,
+  parseTimeText,
+} from "@util/timetable";
 
 const LAB_URL = "http://202.115.175.175",
   LAB_HOME = "/aexp/stuTop.jsp",
@@ -101,7 +108,7 @@ export const fetchLabTimeTable = async (cookie: string) => {
     cookieValue = cookieUtil.parse(cookie)["JSESSIONID"],
     newCookie = cookie + "; " + cookieUtil.serialize("aexpsid", cookieValue);
 
-  const courseList: IJWCommon[] = [];
+  const courseList: ILabCourse[] = [];
   for (let i = 0; i < 50; i++) {
     // 获取课表html文本
     let body = await fetchContext(
@@ -145,19 +152,20 @@ export const fetchLabTimeTable = async (cookie: string) => {
 
     //按课程名建立课程对象
     for (let j = 0; j < tableRowText.length; j++) {
-      const courseArr = tableRowText[j],
-        courseObj: IJWCommon = {} as IJWCommon,
-        courseTime = courseArr[2]
-          .replace(/^(\d+).+?(\d+)-(\d+).+/, "$1 $2 $3")
-          .split(" ");
-      courseObj["week"] = courseTime[0] || "0";
-      courseObj.section_start = courseTime[1] || "0";
-      courseObj.section_end = courseTime[2] || "0";
-      courseObj.jw_course_name = courseArr[0] || "0";
-      courseObj.custom_task = courseArr[1];
-      courseObj.custom_task = courseArr[3];
-      courseObj.base_teacher_name = courseArr[4];
-      courseObj.week_day = String(getWeekFromString(courseArr[2]));
+      const courseArr = tableRowText[j];
+      const courseObj: ILabCourse = {
+        name: courseArr[0],
+        task: courseArr[1],
+        time: courseArr[2],
+        place: courseArr[3],
+        teacher: courseArr[4],
+        type: courseArr[5],
+        seat: courseArr[6],
+        status: courseArr[7],
+        selected: Number(courseArr[8]),
+        min: Number(courseArr[9]),
+        cancel: courseArr[10],
+      };
       courseList.push(courseObj);
     }
 
@@ -183,32 +191,20 @@ export const fetchLabTimeTable = async (cookie: string) => {
       curPageUrl = nxtPageUrl;
     }
   }
-  return covertJwToCommon(courseList);
+  return covertLabCourse(courseList);
 };
 
 /**将汉字星期数转为数字 */
 const getWeekFromString = (week: string) => {
   const arr = ["一", "二", "三", "四", "五", "六", "日"];
-  return 1+arr.findIndex((v) => {
-    return week.includes("星期" + v);
-  });
+  return (
+    1 +
+    arr.findIndex((v) => {
+      return week.includes("星期" + v);
+    })
+  );
 };
 
-/**教务系统返回的课表结构 */
-interface IJWCommon {
-  jw_course_code: string;
-  base_teacher_name: string;
-  base_room_name: string;
-  week: string;
-  jw_task_book_no: string;
-  jw_course_name: string;
-  section_end: string;
-  week_day: string;
-  section: string;
-  base_teacher_no: string;
-  section_start: string;
-  custom_task?: string;
-}
 /**获取当前学期的非实验课 */
 export const fetchCommonTimetable = async (cookie: string) => {
   const cookieValue = cookieUtil.parse(cookie)["JSESSIONID"],
@@ -231,71 +227,6 @@ export const fetchCommonTimetable = async (cookie: string) => {
     body: "op=getJwTimeTable&time=" + new Date(),
   });
 
-  const resJson: IJWCommon[] = JSON.parse(res.body);
-  console.log(covertJwToCommon(resJson));
-  return covertJwToCommon(resJson);
-};
-
-/**本系统使用的课表DTO */
-interface ICommonCourse {
-  code: string;
-  name: string;
-  teacher: string;
-  place: string;
-  time: number[];
-  tasks?: string[];
-}
-
-/**将上课周次和节数转为数字编号 */
-const getNumFromWeekAndSection = (
-  week: number | string,
-  day: number | string,
-  sStart: number | string,
-  sEnd: number | string
-) => {
-  let w = Number(week),
-    s = Number(sStart),
-    d = Number(day),
-    base = (d - 1) * 6 + (w - 1) * 42,
-    e = Number(sEnd),
-    arr: number[] = [];
-  if (w < 0 || w > 30 || s < 0 || s > 12 || d < 0 || d > 7 || e < 0 || e > 12) {
-    return [0];
-  }
-  while (s <= e) {
-    arr.push(base + s);
-    s++;
-  }
-  return arr;
-};
-
-/**将教务系统返回的课表转换为ICommonCourse */
-const covertJwToCommon = (jwList: IJWCommon[]) => {
-  jwList.sort((a, b) => (a.week >= b.week ? -1 : 1));
-  const courseArr: ICommonCourse[] = [];
-  const map = new Map<string, number>();
-  for (const c of jwList) {
-    //只要上课地点和课程号一样，就可以放到同一个ICommonCourse
-    const courseId = c.jw_course_code + c.base_room_name;
-    let index = map.get(courseId);
-    const timeArr = getNumFromWeekAndSection(
-      c.week,
-      c.week_day,
-      c.section_start,
-      c.section_end
-    );
-    if (index === undefined) {
-      map.set(courseId, courseArr.length);
-      courseArr.push({
-        code: c.jw_course_code,
-        name: c.jw_course_name,
-        teacher: c.base_teacher_name,
-        place: c.base_room_name,
-        time: timeArr,
-      });
-    } else {
-      courseArr[index].time.push(...timeArr);
-    }
-  }
-  return courseArr;
+  const resJson: IJWCourse[] = JSON.parse(res.body);
+  return covertJwCourse(resJson);
 };
