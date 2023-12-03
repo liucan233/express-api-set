f96a3dd806df048a4610cf08a98f9e878298604e
liucan <liucan2333@qq.com>
2023-12-03 18:08:22 +0800

    chore: 添加prisma以操作数据库

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # 西科大一站式服务大厅登陆服务
 
-## 主要API
+## 主要 API
 
 参见https://www.apifox.cn/apidoc/shared-a5193ab2-870b-454a-8dc2-683f95680172
 
 ## 开发项目
 
-使用`npm i`安装依赖，`npm run dev`即可。
+先修改 prisma 和 env 目录的环境变量，然后使用`pnpm i`安装依赖，再执行`npx prisma generate`生成数据库类型定义，最后`pnpm start`即可。
 
 ## 部署项目
 
-使用`npm i`安装依赖，`npm run build`构建，`pm2 start dist/index.js`即可。
+需要先同步数据库变更，然后安装依赖，执行`npm run build:x`，然后再运行产物目录的`app.js`，例如`node dist/app.js`。
---
+++ prisma/migrations/20231203090921_impl_comment/migration.sql
@@ -0,0 +1,37 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `CommentList` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `desc` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Comment` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `sourceId` INTEGER NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `replyUserId` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_sourceId_fkey` FOREIGN KEY (`sourceId`) REFERENCES `CommentList`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_replyUserId_fkey` FOREIGN KEY (`replyUserId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
